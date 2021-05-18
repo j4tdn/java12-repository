457ffa24e205444bb8d2cd0cbeec4246b7802093
minhnhat <phancaominhnhat10@gmail.com>
2021-05-18 08:01:07 +0700

    test ctr shift f

--- lesson02-data-structure/src/control/Ex01.java
+++ lesson02-data-structure/src/control/Ex01.java
@@ -2,23 +2,25 @@ package control;
 
 public class Ex01 {
 	public static void main(String[] args) {
-		//tìm max 2 số, 3 số
+		// tìm max 2 số, 3 số
 		int a = 8;
 		int b = 6;
 		int c = 10;
-		
+
 		System.out.println("Max 1; " + max(a, b));
-		System.out.println("Max 2: " + max(a, b,  c));
+		System.out.println("Max 2: " + max(a, b, c));
 	}
+
 	private static int max(int a, int b) {
-		if(a > b) {
+		if (a > b) {
 			return a;
 		}
 		return b;
 	}
+
 	private static int max(int a, int b, int c) {
 		int max = a > b ? a : b;
-		if(c > max) {
+		if (c > max) {
 			max = c;
 		}
 		return max;
