a0f1d9e1ed6cf56b4a977fc2bae6b3f5b675a926
daandden <vuanhng00@gmail.com>
2021-05-25 09:20:11 +0700

    lesson03-oop 25.05.2021

---
+++ lesson03-oop/src/demo/Ex04.java
@@ -0,0 +1,23 @@
+package demo;
+
+import bean.CarAdv;
+
+public class Ex04 {
+	public static void main(String[] args) {
+		// Demo static
+		CarAdv c1 = new CarAdv(1, "Green");
+		CarAdv c2 = new CarAdv(2, "Black");
+		CarAdv c3 = new CarAdv(3, "White");
+		
+		CarAdv.model = "Audi";
+		
+		System.out.println("c1: " + c1);
+		System.out.println("c2: " + c2);
+		System.out.println("c3: " + c3);
+		
+		// static
+		// non-static
+		//khi nhung bien, ham khong quan tam den nhung doi tuong dang goi
+		//thi bien, ham so la static
+	}
+}
