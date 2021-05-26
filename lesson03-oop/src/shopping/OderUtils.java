6c9e7c9e25a4b973b56792b79f6aa584498ae55c
GiaBaone <102190252@sv1.dut.udn.vn>
2021-05-26 23:28:09 +0700

    lesson03-oop shopping 26.5.21

---
+++ lesson03-oop/src/shopping/OderUtils.java
@@ -0,0 +1,35 @@
+package shopping;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+/**
+ * 
+ * Utility of Order class
+ * Normally: Utility is place where contains utility method
+ * No care created class's object
+ * 
+ */
+public class OderUtils {
+	
+	private OderUtils() {
+		
+	}
+	public static double export(Order order) {
+		double totalofMoney = 0;
+		ItemDetail[] ids = order.getItemDetail();
+		// foreach
+		for (ItemDetail id : ids) {
+			Item item = id.getItem();
+			int amount = id.getAmount();
+			double idCost = item.getCost() * amount;
+			if (item.getCost() > 590 && LocalDate.of(2021, Month.MAY, 8).isEqual(order.getDatetime().toLocalDate())) {
+				totalofMoney += idCost * 0.9;
+			} else {
+				totalofMoney += idCost;
+			}
+		}
+
+		return totalofMoney;
+	}
+}
