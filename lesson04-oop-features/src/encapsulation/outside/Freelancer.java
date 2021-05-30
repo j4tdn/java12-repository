fabf3955fcc8e228d78e43b4d3770c3154a5007c
le hoang vu <vubk2017@gmail.com>
2021-05-31 00:22:53 +0700

    Lesson04-oop-features 30.5.2021

---
+++ lesson04-oop-features/src/encapsulation/outside/Freelancer.java
@@ -0,0 +1,17 @@
+package encapsulation.outside;
+
+import encapsulation.inside.Company;
+import encapsulation.inside.Developer;
+
+public class Freelancer extends Developer {
+    
+    public static void main(String[] args) {
+        Company comp = new Company();
+        comp.id = 123;
+        //	comp.name = "mgm";
+        //	comp.address ="Pasteur";
+        //	comp.fund
+        Freelancer fr = new Freelancer();
+        fr.getItSkills();
+    }
+}
