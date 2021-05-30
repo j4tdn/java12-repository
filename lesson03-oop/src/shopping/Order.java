2baf3f5343eff944568f8fed03af937b8c867b1a
DESKTOP-5U1VBJ6\Admin <hoanphana1@gmail.com>
2021-05-30 11:37:50 +0700

    shopping oop 30.5.21

---
+++ lesson03-oop/src/shopping/Order.java
@@ -0,0 +1,41 @@
+package shopping;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+public class Order {
+	private Customer customer;
+	private ItemDetail[] itemDetail;
+	private LocalDateTime datetime; //day-mm-yy h-m-s
+	 public Order() {
+	}
+	public Order(Customer customer, ItemDetail[] itemDetail, LocalDateTime datetime) {
+		super();
+		this.customer = customer;
+		this.itemDetail = itemDetail;
+		this.datetime = datetime;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public ItemDetail[] getItemDetail() {
+		return itemDetail;
+	}
+	public void setItemDetail(ItemDetail[] itemDetail) {
+		this.itemDetail = itemDetail;
+	}
+	public LocalDateTime getDatetime() {
+		return datetime;
+	}
+	public void setDatetime(LocalDateTime datetime) {
+		this.datetime = datetime;
+	}
+	@Override
+	public String toString() {
+		return "Order [customer=" + customer + ", itemDetail=" + Arrays.toString(itemDetail) + ", datetime=" + datetime
+				+ "]";
+	}
+}
