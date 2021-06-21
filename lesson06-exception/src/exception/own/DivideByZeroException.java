d5294fae54127bf77a2599416553d195b4dff911
minhnhat2904 <phancaominhnhat10@gmail.com>
2021-06-21 17:08:21 +0700

    lesson06-exception 20.06.2021

---
+++ lesson06-exception/src/exception/own/DivideByZeroException.java
@@ -0,0 +1,11 @@
+package exception.own;
+
+public class DivideByZeroException extends ArithmeticException {
+
+	/**
+	 *  TODO: serial ID
+	 */
+
+	private static final long serialVersionUID = 8485727497261594436L;
+
+}
