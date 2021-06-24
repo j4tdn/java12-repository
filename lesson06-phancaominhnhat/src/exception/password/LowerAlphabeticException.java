92b939eee1f887c400e36adac96f81c5012e5e87
minhnhat2904 <phancaominhnhat10@gmail.com>
2021-06-24 16:46:18 +0700

    lesson06-phancaominhnhat 24.06.2021

---
+++ lesson06-phancaominhnhat/src/exception/password/LowerAlphabeticException.java
@@ -0,0 +1,13 @@
+package exception.password;
+
+public class LowerAlphabeticException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7016469666671710689L;
+	
+	public LowerAlphabeticException(String message) {
+		super(message);
+	}
+}
---
+++ lesson06-phancaominhnhat/src/exception/password/SpecialCharacterException.java
@@ -0,0 +1,13 @@
+package exception.password;
+
+public class SpecialCharacterException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4202865651097007056L;
+	
+	public SpecialCharacterException(String message) {
+		super(message);
+	}
+}
