d35c8218a339324f557a8bd9cfdc8a82a4220ea6
Le Gia Anh Khoa <102190220@sv1.dut.udn.vn>
2021-07-04 12:42:59 +0700

    lesson08-String 1.07.2021

--- lesson07-libraries/src/utils/DateUtils.java
+++ lesson07-libraries/src/utils/DateUtils.java
@@ -16,32 +16,46 @@ public class DateUtils {
 		// TODO Auto-generated constructor stub
 	}
 
+	public static Calendar getdatetime(int year, int month, int day, int hours, int minutes, int second) {
+		Calendar c = Calendar.getInstance();
+		c.set(year, month, day, hours, minutes, second);
+		return c;
+	}
+
+	public static Calendar getdatetime(int year, int month, int day) {
+		Calendar c = Calendar.getInstance();
+		c.set(year, month, day);
+		return c;
+	}
+
 	public static boolean isLeapYear(int year) {
 		return gc.isLeapYear(year);
 	}
+
 	/**
 	 * Convert calendar to formatted string with dd/MM/yyyy pattern.
+	 * 
 	 * @param c given calendar
 	 * @return formatted calendar as string
 	 */
-	
-	public static String format(Calendar c)
-	{
+
+	public static String format(Calendar c) {
 		Objects.requireNonNull(c);
 		DateFormat df = new SimpleDateFormat(PATTERN);
 		return df.format(c.getTime());
-		
+
 	}
+
 	/**
 	 * Convert string to date with dd/MMM/YYYY KK:mm:ss a EEEE
-	 * @param s given string 
+	 * 
+	 * @param s given string
 	 * @return Date type
 	 */
-	public static Date toDate(String s)
-	{
+	public static Date toDate(String s) {
 		Objects.requireNonNull(s);
 		DateFormat df = new SimpleDateFormat(PATTERN);
-		Date date =null;
+		Date date = null;
 		try {
 			date = df.parse(s);
 		} catch (ParseException e) {
@@ -50,15 +64,16 @@ public class DateUtils {
 		}
 		return date;
 	}
+
 	/**
 	 * Convert date to calendar
+	 * 
 	 * @param date given date
 	 * @return Calendar type
 	 */
-	public static Calendar to(Date date)
-	{
+	public static Calendar to(Date date) {
 		Objects.requireNonNull(date);
-		Calendar c= Calendar.getInstance();
+		Calendar c = Calendar.getInstance();
 		c.setTime(date);
 		return c;
 	}
