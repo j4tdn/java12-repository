9d976606c1cad058739c9d995924b38ac985781e
Nghia Pham <73949420+rinnghia56@users.noreply.github.com>
2021-07-12 20:55:31 +0700

    lesson-08

--- lesson08-String/src/immutable/Ex01.java
+++ lesson08-String/src/immutable/Ex01.java
@@ -14,6 +14,16 @@ public class Ex01 {
 		hash("l2",l2);
 		hash("l3",l3);
 		
+		//Immutable: bất biến , ko thay đổi giá trị
+		//Reality: Immutable for String value in heap
+		System.out.println("=========Immutable=============");
+		
+		String i1="one";    // ô nhớ có giá trị là "one" sẽ đc tạo mới ở heap-constant pool
+		hash("i1",i1);
+		
+		i1="welcome";	// ô nhớ có giá trị là "one" sẽ ko bị cập nhật mà vẫn giữ nguyên trên heap, ô nhớ "two " sẽ được tạo ra
+		hash("i1",i1);
+		
 		System.out.println("======================");
 		String o1=new String("hello");
 		String o2=new String("welcome");
