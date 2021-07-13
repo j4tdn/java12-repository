ce576b4052f972a47db4648340fc170e0565c088
Le Gia Anh Khoa <102190220@sv1.dut.udn.vn>
2021-07-13 13:38:45 +0700

    lesson08-mutable 9.7.2021

--- lesson08-string/src/immutable/Ex04.java
+++ lesson08-string/src/immutable/Ex04.java
@@ -21,7 +21,7 @@ public class Ex04 {
 		String song  = "d:/music/somthingyoulike.mp3";
 		int lastSlash =  song.lastIndexOf("/");
 		int lastDot =song.lastIndexOf(".");
-		String songName =song.substring(lastSlash+1;lastDot);
+		String songName =song.substring(lastSlash+1,lastDot);
 		System.out.println("songName: "+songName);
 		byte a = 65;
 		System.out.println("a: "+(char)a);
