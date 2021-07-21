10537f863c14ad1d5037a36bb325b680400660e2
Le Gia Anh Khoa <102190220@sv1.dut.udn.vn>
2021-07-21 14:50:54 +0700

    lesson10-arrays 19.07.2021

--- lesson10-arrays/src/manipulation/Ex01.java
+++ lesson10-arrays/src/manipulation/Ex01.java
@@ -1,6 +1,7 @@
 package manipulation;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Ex01 {
 	public static void main(String[] args) {
@@ -20,6 +21,58 @@ public class Ex01 {
 		for (int i = k + 1; i < rs.length; i++) {
 			rs[i] = digits[i - 1];
 		}
+
 		return rs;
+	}
+
+	private static int[] add1(int[] digits, int k, int h) {
+		int[] rs = new int[digits.length + 1];
+
+		for (int i = 0; i < rs.length; i++) {
+			int j = i;
+			if (i == k) {
+				rs[k] = h;
+				continue;
+			}
+			if (i > k) {
+
+				j = j - 1;
+			}
+			rs[i] = digits[j];
+		}
+		return rs;
+	}
+
+	private static int[] insert(int[] digits, int k, int newElement) {
+		int[] result = new int[digits.length + 1];
+		for (int i = 0; i < digits.length; i++) {
+			result[i] = digits[i];
+		}
+		// shift right 1 unit
+		for (int j = result.length - 1; j > k; j--) {
+			result[j] = result[j - 1];
+		}
+		result[k] = newElement;
+		return result;
+	}
+
+	private static int[] remove(int[] digits, int k) {
+		int[] result = new int[digits.length + 1];
+		for (int i = 0; i < k; i++) {
+			result[i] = digits[i];
+		}
+		for (int i = k + 1; i < digits.length; i++) {
+			result[i - 1] = digits[i];
+		}
+		return digits;
+
+	}
+
+	private static int[] delete(int[] digits, int k) {
+		for (int i = 0; i < digits.length; i++) {
+			digits[i + 1] = digits[i];
+		}
+		return Arrays.copyOfRange(digits, 0, digits.length - 1);
+
 	}
 }
---
+++ lesson10-arrays/src/sorting/Ex01.java
@@ -0,0 +1,19 @@
+package sorting;
+
+import Utils.ArraysUtils;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		int[] numbers = { 1, 5, 9, 4, 6, 11 };
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = 0; j < numbers.length - i - 1; i++) {
+				if (numbers[j] > numbers[j + 1]) {
+					int tmp = numbers[j];
+					numbers[j] = numbers[j + 1];
+					numbers[j + 1] = tmp;
+				}
+			}
+		}
+		ArraysUtils.printf(numbers);
+	}
+}
