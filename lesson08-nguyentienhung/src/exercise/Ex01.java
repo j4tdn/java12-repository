3ce347f21d8d91ff09490b48033adc562c3883b3
HungHungGNX <79443809+HungHungGNX@users.noreply.github.com>
2021-07-22 16:47:05 +0700

    lesson08-nguyentienhung 22.07.2021

---
+++ lesson08-nguyentienhung/src/exercise/Ex01.java
@@ -0,0 +1,42 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter: ");
+//		String line = scanner.nextLine();	
+		String line = "Bạn có thể chỉ định ngôn ngữ tìm kiếm của mình trong Tùy chọn";
+		scanner.close();
+		printCharacters(line);
+		printWords(line);
+		printReverse(line);
+	}
+
+	private static void printCharacters(String line) {
+		System.out.println("-- Print characters in String --");
+		for (char a : line.toCharArray()) {
+			System.out.println(a);
+		}
+	}
+
+	private static void printWords(String line) {
+		System.out.println("-- Print words in String");
+		for (String a : line.split("\\s+")) {
+			System.out.println(a);
+		}
+	}
+
+	private static void printReverse(String line) {
+		System.out.println("-- Print reverse chars --");
+		for (char a : (new StringBuilder().append(line).reverse().toString().toCharArray())) {
+			System.out.println(a);
+		}
+
+		System.out.println("-- Print reverse words --");
+		for (String a : line.split("\\s+")) {
+			System.out.println((new StringBuilder().append(a).reverse()));
+		}
+	}
+}
