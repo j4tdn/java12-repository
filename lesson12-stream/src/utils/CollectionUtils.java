61ae1edec286a51d5dd352211393667b0bd076c2
GiaBaone <102190252@sv1.dut.udn.vn>
2021-08-10 22:25:08 +0700

    lesson12-stream 10.8.21

---
+++ lesson12-stream/src/utils/CollectionUtils.java
@@ -0,0 +1,14 @@
+package utils;
+
+import java.util.List;
+
+public class CollectionUtils {
+	private CollectionUtils() {
+		// TODO Auto-generated constructor stub
+	}
+	public static <E> void printf(List<E> elements) {
+		for( E e : elements) {
+			System.out.println(e);
+		}
+	}
+}
