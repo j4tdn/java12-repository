cb0d90643c778c09fccf6055dc6c44e91cace40e
GiaBaone <102190252@sv1.dut.udn.vn>
2021-08-16 22:03:11 +0700

    lesson12-lamda expresion 16.8.21

--- lesson12-stream/src/demo/Ex02.java
+++ lesson12-stream/src/demo/Ex02.java
@@ -1,10 +1,14 @@
 package demo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import bean.Apple;
-import bean.Strategy;
 import utils.AppleUtils;
 import utils.CollectionUtils;
 
@@ -21,7 +25,31 @@ public class Ex02 {
 		List<Apple> red = filterApples(inventory, a -> "red".equals(a.getColor()));
 		CollectionUtils.printf(red);
 		
+		//Lamda expresions
+		// Get list of origin countries of apples
+		Set<String> countries = map(inventory, new Function<Apple, String>() {
+			public String apply(Apple t) {
+				return t.getCountry();
+			};
+		});
+		CollectionUtils.printf(countries);
 		
+		List<String> texts = Arrays.asList("abc","abcde","ab","abcdef");
+		Set<Integer> lengths = map(texts, new Function<String, Integer>() {
+			public Integer apply(String s) {
+				return s.length();
+			};
+		});
+		CollectionUtils.printf(lengths);
+		
+	}
+	private static <T,R> Set<R> map(List<T> ts, Function<T, R> function){
+		Set<R> result = new HashSet<>();
+		
+		for(T t:ts) {
+			result.add(function.apply(t));
+		}
+		return result;
 	}
 	/**
 	 * Get apple wwith given condition
@@ -30,7 +58,7 @@ public class Ex02 {
 	 * @param behavior given condition
 	 * @return output data
 	 */
-	private static List<Apple> filterApples(List<Apple> inventory,Strategy<Apple> strategy){
+	private static List<Apple> filterApples(List<Apple> inventory,Predicate<Apple> strategy){
 		List<Apple> rs = new ArrayList<Apple>();
 	
 		for(Apple apple : inventory) {
---
+++ lesson12-stream/src/demo/Ex03.java
@@ -0,0 +1,51 @@
+package demo;
+
+import bean.Apple;
+
+//class Ex03 contains method main, running
+public class Ex03 {
+	//global variable = 
+	public static void main(String[] args) {
+		//local variable
+		
+		
+		
+		//final >> final at stack
+		final int a=10;
+		
+		final Apple a1 = new Apple();
+		final Apple a2 = new Apple();
+		
+		//a1=a2 : not allows
+		a1.setColor("yellow");
+		
+		//final >> primitive: cannot reassign value
+		//final >> object : cannot reassign refence's address
+		Runnable runnbale = run(2000);
+		runnbale.run();
+		
+	}
+	private static Runnable run(int time) {
+		//lexical scope
+		//local variable
+		//outer scope
+		 String jobName = "Queue Job";
+		 Apple jobDetail = new Apple(1, "black", 100, "Lào");
+		
+		//anonymous class >> contains run method
+		//Ex03 and anonymous class are separately
+		return new Runnable() {
+			@Override
+			public void run() {
+				//local variable
+				//inner scope
+				int time = 2000;
+				//jobName = "stack job";
+				jobDetail.setColor("white");
+				System.out.println(jobName+ "is running about " + time + "ms");
+				System.out.println("jobDetail: " + jobDetail);
+			}
+		};
+		
+	}
+}
