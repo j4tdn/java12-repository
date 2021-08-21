69bf5323136e5f2a17b39246dbf861ff6ca0b339
DESKTOP-5U1VBJ6\Admin <hoanphana1@gmail.com>
2021-08-21 22:37:54 +0700

    stream 21.8.2021

---
+++ lesson12-stream/src/demo/Ex08.java
@@ -0,0 +1,29 @@
+package demo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import utils.ArrayUtils;
+import utils.CollectionUtils;
+
+public class Ex08 {
+	public static void main(String[] args) {
+		// source: List<E> , E[]
+		int[] digits = {1,2,1,3,3,2,4};
+		
+		int[] evenNums = Arrays.stream(digits).filter(nbr -> nbr % 2 ==0)
+						.distinct().toArray();
+		ArrayUtils.printf(evenNums);
+		
+		/*List<Integer> numbers = Arrays.asList(1,2,1,3,3,2,4);
+		
+		Set<Integer> evenNums = numbers.stream()
+									.filter(nbr -> nbr % 2 ==0)
+									//.distinct()  //Set, Map, HashTable >> hashcode, equal
+									.collect(Collectors.toSet());
+		CollectionUtils.printf(evenNums);
+	*/
+	}
+}
