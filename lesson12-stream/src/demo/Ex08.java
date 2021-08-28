fc22daf0663a0d66e4a7d0e325fc5377c1c7d68b
DESKTOP-5U1VBJ6\Admin <hoanphana1@gmail.com>
2021-08-28 22:38:31 +0700

    stream 28.8.2021

--- lesson12-stream/src/demo/Ex08.java
+++ lesson12-stream/src/demo/Ex08.java
@@ -1,6 +1,7 @@
 package demo;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -25,5 +26,13 @@ public class Ex08 {
 									.collect(Collectors.toSet());
 		CollectionUtils.printf(evenNums);
 	*/
+		System.out.println("====================");
+		//convert int[] -> List<Integer>
+		List<Integer> numbers = Arrays.stream(digits).boxed().collect(Collectors.toList());
+		
+		List<Integer> uniqueNums = numbers.stream()
+				.filter(nbr -> Collections.frequency(numbers, nbr)==1)
+				.collect(Collectors.toList());
+		CollectionUtils.printf(uniqueNums);
 	}
 }
