b15dc529efcf77d9722b7c3263ba3cbce5fd4ebb
le hoang vu <vubk2017@gmail.com>
2021-09-13 09:47:11 +0700

    lesson13-files  12.09.2021

---
+++ lesson13-files/src/manipulation/Ex03.java
@@ -0,0 +1,23 @@
+package manipulation;
+
+import common.Trader;
+import utils.DataUtils;
+import utils.FileUtils;
+
+import java.io.File;
+import java.util.List;
+
+public class Ex03 {
+    private static final String TRADER_PATH = "trader.txt";
+
+    public static void main(String[] args) {
+        List<Trader> traders = DataUtils.getTrader();
+
+        File file = new File(TRADER_PATH);
+        //FileUtils.writeObject(file,traders);
+
+        List<Trader> a = FileUtils.readObject(file);
+        System.out.println(a);
+    }
+
+}
