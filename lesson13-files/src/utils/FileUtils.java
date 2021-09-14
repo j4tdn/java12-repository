76c61e63c69ccb2d9bf5013024f7fadc836d90cf
DESKTOP-5U1VBJ6\Admin <hoanphana1@gmail.com>
2021-09-14 08:26:06 +0700

    files 12.9.2021

--- lesson13-files/src/utils/FileUtils.java
+++ lesson13-files/src/utils/FileUtils.java
@@ -4,18 +4,43 @@ import java.awt.Desktop;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import common.Extension;
+import common.FileHandler;
 
 public class FileUtils {
 	private static final Random rd = new Random();
 	
 	private FileUtils() {
 		
+	}
+	public static <E extends FileHandler> void writeLines(Path path, List<E> list, OpenOption option) {
+		
+		List<String> lines = list.stream()
+							.map(E::toLine)
+							.collect(Collectors.toList());
+		try {
+			Files.write(path,lines,option);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Write file successfull");
 	}
 	public static void read(File file) {
 		//read file >> draft
@@ -37,6 +62,36 @@ public class FileUtils {
 			FileUtils.close(br,fr);
 		}
 	}
+	
+	public static <E> List<E> readLines(Path path, Function<String, E> func) {
+		try {
+			List<String> lines = Files.readAllLines(path);
+			return lines.stream().map(func)
+					.collect(Collectors.toList());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Collections.emptyList();
+	}
+	public static <T> List<T> readObject(File file){
+		FileInputStream fis=null;
+		ObjectInputStream ois=null;
+		try {
+			fis = new FileInputStream(file);
+			ois = new ObjectInputStream(fis);
+			
+			return safeList(ois.readObject());
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return Collections.emptyList();
+	}
+	@SuppressWarnings("unchecked")
+	private static <T> List<T> safeList(Object o){
+		return(List<T>) o;
+	}
+	
 	public static void write(File file) {
 		//write file >> draft
 		//write file
@@ -57,6 +112,20 @@ public class FileUtils {
 		
 		System.out.println("=========End========");
 		FileUtils.open(file);
+	}
+	
+	public static void writeObject(File file, Object o) {
+		FileOutputStream fos= null;
+		ObjectOutputStream oos=null;
+		try {
+			fos = new FileOutputStream(file);
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(o);
+			oos.close();
+			fos.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 	public static File create(String path) {
 		boolean isValid = false;
@@ -122,4 +191,5 @@ public class FileUtils {
 			}
 		}
 	}
+	
 }
