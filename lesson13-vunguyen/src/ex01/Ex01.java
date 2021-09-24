10ad03f8ef971a9ebbb7fdc4ca33def9709e468e
daandden <vuanhng00@gmail.com>
2021-09-24 19:56:37 +0700

    lesson13-vunguyen fix ex01 24.09.2021

--- lesson13-vunguyen/src/ex01/Ex01.java
+++ lesson13-vunguyen/src/ex01/Ex01.java
@@ -5,6 +5,10 @@ import utils.FileUtils;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import static common.Extension.jpeg;
 import static common.Extension.jpg;
@@ -16,7 +20,7 @@ public class Ex01 {
     private static final String SYSTEM_PATH = "garbage/system";
     private static final String CODING_PATH = "garbage/coding";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File garbage = FileUtils.createDir(GARBAGE_PATH);
         File image = FileUtils.createDir(IMAGE_PATH);
         File music = FileUtils.createDir(MUSIC_PATH);
@@ -38,7 +42,7 @@ public class Ex01 {
 
         // move .bat to system
         moveFile(garbage, system,
-                 (f, name) -> name.endsWith(Extension.java.val()));
+                 (f, name) -> name.endsWith(Extension.bat.val()));
 
         // copy .java to coding
         copyFile(garbage, coding, (f, name) -> name.endsWith(Extension.java.val()));
@@ -59,7 +63,6 @@ public class Ex01 {
         File[] files = dir.listFiles(filter);
         for (File file : files) {
             file.renameTo(new File(targetDir, file.getName()));
-            file.delete();
         }
     }
 
@@ -70,17 +73,19 @@ public class Ex01 {
      * @param targetDir target directory
      * @param filter    FilenameFilter filter file by name
      */
-    public static void copyFile(File dir, File targetDir, FilenameFilter filter) {
+    public static void copyFile(File dir, File targetDir, FilenameFilter filter) throws IOException {
         File[] files = dir.listFiles(filter);
         for (File file : files) {
-            file.renameTo(new File(targetDir, file.getName()));
+            System.out.println(file.getPath());
+            System.out.println(targetDir.getPath());
+            Files.copy(file.toPath(), Paths.get(targetDir.getPath(), file.getName()), StandardCopyOption.REPLACE_EXISTING);
         }
     }
 
     /**
      * Delete file
      *
-     * @param dir directory to copy
+     * @param dir    directory to copy
      * @param filter filter by name
      */
     public static void deleteFile(File dir, FilenameFilter filter) {
